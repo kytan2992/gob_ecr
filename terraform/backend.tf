087389b8132ec8d40bd4f0cf91cad59072210b6b
kytan2992 <kytan2992@gmail.com>
2025-05-06 18:20:33 +0800

    initial commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "ky-s3-terraform"
-    key    = "ky-tf-fill-here.tfstate"
+    key    = "ky-tf-gob-ecr.tfstate"
     region = "us-east-1"
   }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,68 @@
+locals {
+  name_prefix = "ky-tf"
+}
 
+resource "aws_ecr_repository" "emailservice" {
+  name         = "${local.name_prefix}-emailservice"
+  force_delete = true
+}
+
+resource "aws_ecr_repository" "checkout" {
+  name         = "${local.name_prefix}-checkout"
+  force_delete = true
+}
+
+resource "aws_ecr_repository" "recommendationservice" {
+  name         = "${local.name_prefix}-recommendationservice"
+  force_delete = true
+}
+
+resource "aws_ecr_repository" "frontend" {
+  name         = "${local.name_prefix}-frontend"
+  force_delete = true
+}
+
+resource "aws_ecr_repository" "paymentservice" {
+  name         = "${local.name_prefix}-paymentservice"
+  force_delete = true
+}
+
+resource "aws_ecr_repository" "productcatalog" {
+  name         = "${local.name_prefix}-productcatalog"
+  force_delete = true
+}
+
+resource "aws_ecr_repository" "cartservice" {
+  name         = "${local.name_prefix}-cartservice"
+  force_delete = true
+}
+
+resource "aws_ecr_repository" "redis" {
+  name         = "${local.name_prefix}-redisalpine"
+  force_delete = true
+}
+
+resource "aws_ecr_repository" "busybox" {
+  name         = "${local.name_prefix}-busybox"
+  force_delete = true
+}
+
+resource "aws_ecr_repository" "loadgenerator" {
+  name         = "${local.name_prefix}-loadgenerator"
+  force_delete = true
+}
+
+resource "aws_ecr_repository" "currencyservice" {
+  name         = "${local.name_prefix}-currencyservice"
+  force_delete = true
+}
+
+resource "aws_ecr_repository" "shippingservice" {
+  name         = "${local.name_prefix}-shippingservice"
+  force_delete = true
+}
+
+resource "aws_ecr_repository" "adservice" {
+  name         = "${local.name_prefix}-adservice"
+  force_delete = true
+}
